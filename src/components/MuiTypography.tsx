2881b29b97c9afbb3f295d3f2b181869205e567b
JeremyDelorme <jeremydelorme23.7@gmail.com>
2023-10-29 18:11:56 -0400

    first commit - Text fields

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material'
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant='h1'>h1 Heading</Typography>
+            <Typography variant='h2'>h2 Heading</Typography>
+            <Typography variant='h3'>h3 Heading</Typography>
+            <Typography variant='h4'>h4 Heading</Typography>
+            <Typography variant='h5'>h5 Heading</Typography>
+            <Typography variant='h6'>h6 Heading</Typography>
+            <Typography variant='subtitle1'>Sub Title 1</Typography>
+            <Typography variant='subtitle2'>Sub Title 2</Typography>
+
+            <Typography variant='body1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nam, architecto natus animi officiis, ea aperiam laboriosam distinctio, incidunt doloremque eos corrupti facere corporis unde deserunt quasi dicta eius! Temporibus.</Typography>
+            <Typography variant='body2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos repellat, maiores illum dolor a blanditiis corporis quia vitae architecto eum dicta quasi inventore animi maxime nostrum deleniti modi consequuntur doloribus!</Typography>
+        </div>
+    )
+}
